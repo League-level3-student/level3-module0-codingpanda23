614bb37e43fa8796447e77c1ece460a0f5ea4836
codingpanda23 <league@WTS20.attlocal.net>
2019-02-23 15:56:13 -0800

    ArrayCheatSheet; RobotRace(IP)

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -7,29 +7,50 @@ public class _00_ArrayCheatSheet {
 		//1. make an array of 5 Strings
 			String[] purples = {"mauve","lavender","plum","sangria","periwinkle"};
 		//2. print the third element in the array
-			//System.out.println(purples[2]);
+			System.out.println(purples[2]);
 		//3. set the third element to a different value
 			purples[2]="iris";
 		//4. print the third element again
 			System.out.println(purples[2]);
 		//5. use a for loop to set all the elements in the array to a string of your choice
 			for (int i = 0; i < purples.length; i++) {
-				
+				String shades = purples[i];
 			}
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+			for (int i = 0; i < purples.length; i++) {
+				System.out.println(purples[i]);
+			}
 		//7. make an array of 50 integers
-
+				int[] ints = new int[50];
 		//8. use a for loop to make every value of the integer array a random number
-
-		//9. without printing the entire array, print only the smallest number on the array
-
+				Random r = new Random();
+				for (int i = 0; i < ints.length; i++) {
+					ints[i] = r.nextInt(51);
+				}
+				
+		//9. without printing the entire array, print only the smallest number in the array
+				int small = 51;
+				for (int i = 0; i < ints.length; i++) {
+					if (small > ints[i]) {
+						small = ints[i];
+					}
+				}
+				System.out.println(small);
 		//10 print the entire array to see if step 8 was correct
-
+				for (int i = 0; i < ints.length; i++) {
+					System.out.println(ints[i]);
+				}
 		//11. print the largest number in the array.
-		
+				int large = 51;
+				for (int i = 0; i < ints.length; i++) {
+					if (large < ints[i]) {
+						large = ints[i];
+					}
+				}
+				System.out.println(large);
+				
 		//12. print only the last element in the array
-		
+				System.out.println(ints[ints.length-1]);
 	}
 }
--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -8,16 +8,25 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
-	
+		public static void main(String[] args) {
 		//2. create an array of 5 robots.
-
+			Robot[] ro = new Robot[5];
 		//3. use a for loop to initialize the robots.
-
+			for (int i = 0; i < ro.length; i++) {
+				ro[i] = new Robot();
+			}
 			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
+				ro[0].moveTo(75, 550);
+				ro[1].moveTo(250, 550);
+				ro[2].moveTo(425, 550);
+				ro[3].moveTo(600, 550);
+				ro[4].moveTo(775, 550);
+				
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
-    	
+				for (int i = 0; i < ro.length; i++) {
+					
+				}
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 		//7. declare that robot the winner and throw it a party!
@@ -26,5 +35,5 @@ public class _01_RobotRace {
     	
 	    //9. make the robots race around a circular track.
 
-	
+	}
 }
