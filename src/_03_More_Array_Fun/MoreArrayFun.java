d686c20f1a2d346b173c241a65d2ce25ac02d9f3
codingpanda23 <league@WTS14.attlocal.net>
2019-03-30 15:32:06 -0700

    MoreArrayFun

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -1,22 +1,55 @@
 package _03_More_Array_Fun;
 
+import java.util.Random;
+
 public class MoreArrayFun {
+
 	// 1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
-		// 2. Write a method that takes an array of Strings and prints all the Strings
-		// in the array.
+		// printStrings();
+		// reverseStrings();
+		// everyOther();
+		// randomOrder();
+	}
 
-		// 3. Write a method that takes an array of Strings and prints all the Strings
-		// in the array
-		// in reverse order.
-
-		// 4. Write a method that takes an array of Strings and prints every other
-		// String in the array.
-
-		// 5. Write a method that takes an array of Strings and prints all the Strings
-		// in the array
-		// in a completely random order. Almost every run of the program should result
-		// in a different order.
+	// 2. Write a method that takes an array of Strings and prints all the Strings
+	// in the array.
+	public static void printStrings() {
+		String stuff[] = { "one", "two", "three", "four", "five" };
+		for (int i = 0; i < stuff.length; i++) {
+			System.out.println(stuff[i]);
+		}
 
 	}
+
+	// 3. Write a method that takes an array of Strings and prints all the Strings
+	// in the array in reverse order.
+	public static void reverseStrings() {
+		String stuff[] = { "one", "two", "three", "four", "five" };
+		for (int i = 4; i < stuff.length; i--) {
+			System.out.println(stuff[i]);
+		}
+	}
+
+	// 4. Write a method that takes an array of Strings and prints every other
+	// String in the array.
+	public static void everyOther() {
+		String stuff[] = { "one", "two", "three", "four", "five" };
+		for (int i = 0; i < stuff.length; i += 2) {
+			System.out.println(stuff[i]);
+		}
+	}
+
+	// 5. Write a method that takes an array of Strings and prints all the Strings
+	// in the array in a completely random order. Almost every run of the program
+	// should result in a different order.
+	public static void randomOrder() {
+		String stuff[] = { "one", "two", "three", "four", "five" };
+		Random rand = new Random();
+		for (int i = 0; i < stuff.length; i++) {
+			int r = rand.nextInt(5);
+			System.out.println(stuff[r]);
+		}
+	}
+
 }
